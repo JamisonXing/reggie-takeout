8bc54e9e545f165e58c6f1ebafb7e95ada09419b
jamison <2810947907@qq.com>
2022-11-22 23:46:41 +0800

    global exception handler demo

---
+++ src/main/java/org/itheima/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package org.itheima.reggie.common;
+
+//异常太多统一处理
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理，底层是代理
+ */
+@ControllerAdvice(annotations = {RestController.class, Controller.class}) //拦截RestController和Controller注解的controller
+@ResponseBody   //返回json数据
+@Slf4j
+public class GlobalExceptionHandler {
+    /**
+     * 异常处理方法
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        log.error(ex.getMessage());
+        return R.error("失败");
+    }
+}
