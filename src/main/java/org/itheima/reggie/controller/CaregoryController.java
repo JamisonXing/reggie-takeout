fdb6f280b5e87586358f589b180c430fe4cc8f5c
jamison <2810947907@qq.com>
2023-02-22 15:27:33 +0800

    修改分类

--- src/main/java/org/itheima/reggie/controller/CaregoryController.java
+++ src/main/java/org/itheima/reggie/controller/CaregoryController.java
@@ -59,4 +59,13 @@ public class CaregoryController {
         categoryService.remove(id);
         return R.success("分类信息删除成功");
     }
+
+    @PutMapping
+    public R<String> update(@RequestBody Category category) {
+        log.info("修改分类信息：{}", category);
+
+        categoryService.updateById(category);
+
+        return R.success("修改信息成功");
+    }
 }
