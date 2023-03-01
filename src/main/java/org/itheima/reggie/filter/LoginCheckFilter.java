c3e7e8463b0d3e0e04168950ebf2f0481f987fa8
jamison <2810947907@qq.com>
2023-03-01 21:13:46 +0800

    短信验证码

--- src/main/java/org/itheima/reggie/filter/LoginCheckFilter.java
+++ src/main/java/org/itheima/reggie/filter/LoginCheckFilter.java
@@ -34,8 +34,13 @@ public class LoginCheckFilter implements Filter {
                 "/employee/login",
                 "/employee/logout",
 
-                "/backend/**",  //静态页面不处理
-                "/front/**"
+                //静态页面不处理
+                "/backend/**",
+                "/front/**",
+
+                //移动端发送短信
+                "/user/sendMsg",
+                "/user/login"
         };
 
         //2.判断本次请求是否需要处理
@@ -48,7 +53,7 @@ public class LoginCheckFilter implements Filter {
             return;
         }
 
-        //4.判断登录状态，已经登录就放行
+        //4-1.判断登录状态，已经登录就放行
         if (request.getSession().getAttribute("employee") != null) {
             log.info("用户已登录,用户id为:{}",request.getSession().getAttribute("employee"));
             Long empId = (Long)request.getSession().getAttribute("employee");
@@ -56,6 +61,16 @@ public class LoginCheckFilter implements Filter {
             filterChain.doFilter(request, response);
             return;
         }
+
+        //4-1.判断登录状态，已经登录就放行
+        if (request.getSession().getAttribute("user") != null) {
+            log.info("移动端用户已登录,用户id为:{}",request.getSession().getAttribute("user"));
+            Long userId = (Long)request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         log.info("用户未登录");
         //5.未登录则返回未登录结果，输出流方式向客户端页面响应数据,前端js根据数据决定是否跳转
         response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
