732839a01d1856b77de6fd798eb99a7e5cbdcd8f
jamison <2810947907@qq.com>
2023-03-07 14:39:24 +0800

    购物车添加与删除

---
+++ src/main/java/org/itheima/reggie/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,11 @@
+package org.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.itheima.reggie.entity.ShoppingCart;
+import org.itheima.reggie.mapper.ShoppingCartMapper;
+import org.itheima.reggie.service.ShoppingCartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShoppingCartServiceImpl extends ServiceImpl<ShoppingCartMapper, ShoppingCart> implements ShoppingCartService{
+}
